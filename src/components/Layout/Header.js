7cec755d0b70ec177e044910b5182fe1de0a5680
wnumik <majkel216@gmail.com>
2022-09-08 15:27:54 +0200

    added cart and modal component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,22 +1,20 @@
-import React, {Component, Fragment} from 'react';
+import React, {Fragment} from 'react';
 import foodImage from '../../assets/food.jpeg';
 import classes from './Header.module.css';
 import HeaderCartButton from "./HeaderCartButton";
 
-class Header extends Component {
-    render() {
+const Header = (props) => {
         return (
             <Fragment>
                 <header className={classes.header}>
                     <h1>./React Food App</h1>
-                    <HeaderCartButton>Cart</HeaderCartButton>
+                    <HeaderCartButton onClick={props.onShowCart}/>
                 </header>
                     <div className={classes['main-image']}>
                         <img src={foodImage} alt="Food"/>
                     </div>
             </Fragment>
         );
-    }
 }
 
 export default Header;
