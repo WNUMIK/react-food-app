ee094e3f4478243d8f642bc5a158fba432c0c387
wnumik <majkel216@gmail.com>
2022-09-08 14:00:02 +0200

    added header

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React, {Component, Fragment} from 'react';
+import foodImage from '../../assets/food.jpeg';
+import classes from './Header.module.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <Fragment>
+                <header className={classes.header}>
+                    <h1>./React Food App</h1>
+                    <button>Cart</button>
+                </header>
+                    <div className={classes['main-image']}>
+                        <img src={foodImage} alt="Food"/>
+                    </div>
+            </Fragment>
+        );
+    }
+}
+
+export default Header;
