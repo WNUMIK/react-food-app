fc270b9a51785e400d31b43d0be2e29d7d9e9a93
wnumik <majkel216@gmail.com>
2022-09-08 18:03:37 +0200

    added bumping button for UX purpose

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,36 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCartButton.module.css';
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+    const [btnIsBumping, setBtnIsBumping] = useState(false);
     const cartCtx = useContext(CartContext);
+    const {items} = cartCtx;
 
-    const numberOfCartItems = cartCtx.items.reduce((currNumber, item) => {
+    const numberOfCartItems = items.reduce((currNumber, item) => {
         return currNumber + item.amount;
     }, 0)
+
+
+    const btnClasses = `${classes.button} ${btnIsBumping ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (cartCtx.items.length === 0) {
+            return;
+        }
+        setBtnIsBumping(true);
+        const timer = setTimeout(() => {
+            setBtnIsBumping(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+
+    }, [items]);
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon/>
             </span>
